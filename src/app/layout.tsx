11693a4a7b8c2a8d3ef4aad46c905ad7144619e4
CodeByArtem <smailxxxvizde@gmail.com>
2025-02-03 11:56:12 +0100

    scss add add style-noiz add fonts

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,21 +1,7 @@
 "use client"
-
-import { Geist, Geist_Mono } from "next/font/google";
-import "./globals.css";
+import "../style/globals.scss";
 import { Provider } from 'react-redux';
 import { store } from '../store/store';
-
-
-const geistSans = Geist({
-  variable: "--font-geist-sans",
-  subsets: ["latin"],
-});
-
-const geistMono = Geist_Mono({
-  variable: "--font-geist-mono",
-  subsets: ["latin"],
-});
-
 
 
 export default function RootLayout({
@@ -25,9 +11,7 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <body
-        className={`${geistSans.variable} ${geistMono.variable} antialiased`}
-      >
+      <body>
       <Provider store={store}>
         {children}
 
