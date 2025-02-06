96321178ec392df3265e4deaa9a3850a1adcbfd9
CodeByArtem <smailxxxvizde@gmail.com>
2025-02-06 22:04:14 +0100

    fix back adress

--- src/services/api.ts
+++ src/services/api.ts
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 export const apiClient = axios.create({
-    baseURL: "https://nestbackgame.onrender.com/api",
+    baseURL: process.env.NEXT_PUBLIC_API_BASE_URL,
     headers: {
         "Content-Type": "application/json",
     },
